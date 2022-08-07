dacc24fa08a5dfd00483ade25d578d7e2443769d
karimch21 <anonKM1999@mail.ru>
2022-08-07 19:50:35 +0300

    fix: fixed url block quotes and url songs

--- stage1-tasks-momentum/js/playList.js
+++ stage1-tasks-momentum/js/playList.js
@@ -1,22 +1,22 @@
 const playList = [
     {
         title: 'Ennio Morricone',
-        src: 'stage1-tasks-momentum/assets/sounds/Ennio Morricone.mp3', 
+        src: './assets/sounds/Ennio Morricone.mp3', 
         duration: 0
     },
     {
         title: 'Aqua Caelestis',
-        src: 'stage1-tasks-momentum/assets/sounds/Aqua Caelestis.mp3',
+        src: './assets/sounds/Aqua Caelestis.mp3',
         duration: '00:58'
     },
     {
         title: 'River Flows In You',
-        src: 'stage1-tasks-momentum/assets/sounds/River Flows In You.mp3',
+        src: './assets/sounds/River Flows In You.mp3',
         duration: '03:50'
     },
     {
         title: 'Summer Wind',
-        src: 'stage1-tasks-momentum/assets/sounds/Summer Wind.mp3',
+        src: './assets/sounds/Summer Wind.mp3',
         duration: '0'
     },
 ]
